0d62094e74ca1b4033921b513efa0c736246a942
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2023-07-16 00:27:22 +0300

    Add opportunity register.

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const authRoute = require("./routes/auth")
 
 
 dotenv.config();
+app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
 }).then(console.log("Connected to Mongo DB"))
     .catch((err) => console.log('err', err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running.");
