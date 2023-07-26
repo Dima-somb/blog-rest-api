73d903afe66a32c09d2071aaeb0225db8ad59e25
NataliaJava <105055910+NataliaJava@users.noreply.github.com>
2023-07-26 06:57:12 +0300

    Unresolved issue.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const bodyParser = require("body-parser");
+const path = require('path');
+const fs = require("fs");
+const imageModel = require("./models/Image");
 
 app.use(
     cors({
@@ -23,7 +27,48 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("Connected to Mongo DB"))
     .catch((err) => console.log('err', err));
 
-const storage = multer.diskStorage({
+app.use(bodyParser.urlencoded(
+    { extended:true }
+))
+
+app.set("view engine","ejs");
+
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+var upload = multer({ storage: storage })
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+app.post("/uploadphoto",upload.single('myImage'),(req,res)=>{
+    var img = fs.readFileSync(req.file.path);
+    var encode_img = img.toString('base64');
+    var final_img = {
+        contentType:req.file.mimetype,
+        image:new Buffer(encode_img,'base64')
+    };
+    imageModel.create(final_img,function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(result.img.Buffer);
+            console.log("Saved To database");
+            res.contentType(final_img.contentType);
+            res.send(final_img.image);
+        }
+    })
+})
+
+/*const storage = multer.diskStorage({
     destination:(req, file, cb) => {
         cb(null, "images");
     },
@@ -35,12 +80,13 @@ const storage = multer.diskStorage({
 const upload = multer({storage: storage});
 app.post("/api/upload", upload.single("file"), (req, res) => {
     res.status(200). json("File has been uploaded");
-})
+})*/
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+app.use("/api/uploadphoto", imageModel);
 
 app.listen("5000", () => {
     console.log("Backend is running.");
