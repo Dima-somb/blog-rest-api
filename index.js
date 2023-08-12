4a2b7f03e56eaa915e9b366839acdf101e10ce6b
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2023-08-12 23:11:23 +0300

    Some fixes for upload.

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const cors = require("cors");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-// const multer = require("multer");
 const bodyParser = require("body-parser");
-// const path = require('path');
 const Image = require("./models/Image");
+const multer = require('multer');
+const path = require('path');
 
 var router = express.Router();
 
@@ -22,6 +22,7 @@ app.use(
 
 dotenv.config();
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -41,13 +42,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
-//
-// app.listen("6000", () => {
-//     console.log("Backend is running.");
-// })
-
-const multer = require('multer');
-const path = require('path');
 
 // Configure body-parser to parse JSON data
 app.use(bodyParser.json());
@@ -62,6 +56,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
 // POST endpoint to handle incoming data
 app.post('/api/posts', upload.single('image'), (req, res) => {
